b03568305439171a274c45f1f8e39149d7baa1fb
pguse <pguse@albertcollege.ca>
2019-12-22 18:12:58 -0500

    finished part-1

--- day6part1/day6part1.go
+++ day6part1/day6part1.go
@@ -11,16 +11,28 @@ func main() {
 
 	var orbit []string
 
-	lines := []string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"}
-	//lines := readLines("day06.txt")
+	//lines := []string{"COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"}
+	lines := readLines("day06.txt")
 
-	planets := make(map[string]int)
+	planets := make(map[string]string)
 
+	// Add all planets to the map
 	for _, p := range lines {
 		orbit = strings.Split(p, ")")
-		planets[orbit[0]] = 0
-		fmt.Printf("%v : %v\n", orbit[0], orbit[1])
+		planets[orbit[1]] = orbit[0]
 	}
+
+	count := 0
+	for key, _ := range planets {
+		for {
+			if planets[key] == "" {
+				break
+			}
+			count++
+			key = planets[key]
+		}
+	}
+	fmt.Printf("Number of orbits = %v", count)
 }
 
 // readFile creates a splice of strings representing each line of the file
